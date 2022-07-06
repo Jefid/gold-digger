430d52cca91c7b8152d08f76de37bed3ae6e8cb8
Jefid <99925114+Jefid@users.noreply.github.com>
2022-07-06 18:21:46 -0400

    creating object stores in idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,9 +1,10 @@
 let db;
-const request = indexedDB.open('pizza_hunt', 1);
+const request = indexedDB.open('money_db', 1);
 
 request.onupgradeneeded = function(event) {
   const db = event.target.result;
-  db.createObjectStore('new_pizza', { autoIncrement: true });
+  db.createObjectStore('new_deposite', { autoIncrement: true });
+  db.createObjectStore('new_expense', { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
@@ -12,63 +13,63 @@ request.onsuccess = function(event) {
 
   // check if app is online, if yes run checkDatabase() function to send all local db data to api
   if (navigator.onLine) {
-    uploadPizza();
+    // uploadPizza();
   }
 };
 
-request.onerror = function(event) {
-  // log error here
+ request.onerror = function(event) {
+ // log error here
   console.log(event.target.errorCode);
 };
 
-function saveRecord(record) {
-  const transaction = db.transaction(['new_pizza'], 'readwrite');
+// function saveRecord(record) {
+//   const transaction = db.transaction(['new_pizza'], 'readwrite');
 
-  const pizzaObjectStore = transaction.objectStore('new_pizza');
+//   const pizzaObjectStore = transaction.objectStore('new_pizza');
 
-  // add record to your store with add method.
-  pizzaObjectStore.add(record);
-}
+//   // add record to your store with add method.
+//   pizzaObjectStore.add(record);
+// }
 
-function uploadPizza() {
-  // open a transaction on your pending db
-  const transaction = db.transaction(['new_pizza'], 'readwrite');
+// function uploadPizza() {
+//   // open a transaction on your pending db
+//   const transaction = db.transaction(['new_pizza'], 'readwrite');
 
-  // access your pending object store
-  const pizzaObjectStore = transaction.objectStore('new_pizza');
+//   // access your pending object store
+//   const pizzaObjectStore = transaction.objectStore('new_pizza');
 
-  // get all records from store and set to a variable
-  const getAll = pizzaObjectStore.getAll();
+//   // get all records from store and set to a variable
+//   const getAll = pizzaObjectStore.getAll();
 
-  getAll.onsuccess = function() {
-    // if there was data in indexedDb's store, let's send it to the api server
-    if (getAll.result.length > 0) {
-      fetch('/api/pizzas', {
-        method: 'POST',
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(response => response.json())
-        .then(serverResponse => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
+//   getAll.onsuccess = function() {
+//     // if there was data in indexedDb's store, let's send it to the api server
+//     if (getAll.result.length > 0) {
+//       fetch('/api/pizzas', {
+//         method: 'POST',
+//         body: JSON.stringify(getAll.result),
+//         headers: {
+//           Accept: 'application/json, text/plain, */*',
+//           'Content-Type': 'application/json'
+//         }
+//       })
+//         .then(response => response.json())
+//         .then(serverResponse => {
+//           if (serverResponse.message) {
+//             throw new Error(serverResponse);
+//           }
 
-          const transaction = db.transaction(['new_pizza'], 'readwrite');
-          const pizzaObjectStore = transaction.objectStore('new_pizza');
-          // clear all items in your store
-          pizzaObjectStore.clear();
-        })
-        .catch(err => {
-          // set reference to redirect back here
-          console.log(err);
-        });
-    }
-  };
-}
+//           const transaction = db.transaction(['new_pizza'], 'readwrite');
+//           const pizzaObjectStore = transaction.objectStore('new_pizza');
+//           // clear all items in your store
+//           pizzaObjectStore.clear();
+//         })
+//         .catch(err => {
+//           // set reference to redirect back here
+//           console.log(err);
+//         });
+//     }
+//   };
+// }
 
 // listen for app coming back online
 window.addEventListener('online', uploadPizza);
